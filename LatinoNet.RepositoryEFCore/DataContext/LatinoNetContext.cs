0537fdb7b6e23bc21205b0b11b394f39815cc901
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-10-23 17:36:06 -0500

    Agregar archivos de proyecto.

---
+++ LatinoNet.RepositoryEFCore/DataContext/LatinoNetContext.cs
@@ -0,0 +1,21 @@
+﻿using LatinoNET.Entities.POCOs;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatinoNet.RepositoryEFCore.DataContext
+{
+    public class LatinoNetContext: DbContext
+    {
+        public LatinoNetContext(
+            DbContextOptions<LatinoNetContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
